4a48dbdd4a9599b5b181b616b716dabf0e95a9ff
khwilo <khwilowatai@gmail.com>
2018-12-17 21:55:27 +0300

    [Feature #162708514] Register the authentication blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 '''Application module'''
 import json
 
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, make_response
 
 from instance.config import APP_CONFIG
 from app.api.v1.models import UserModel
+
+from app.api.v1.views import AUTH
 
 def create_app(config_name):
     '''Instantiate the Flask application'''
@@ -13,29 +15,7 @@ def create_app(config_name):
     app.config.from_object(APP_CONFIG[config_name])
     app.config.from_pyfile('config.py')
 
-    @app.route('/auth/signup', methods=['POST'])
-    def user_registration():
-        '''API endpoint for user registration'''
-        username = str(request.args.get('username'))
-        email = str(request.args.get('email'))
-        password = UserModel.generate_password_hash(str(request.args.get('password')))
-
-        # Create an instance of the user
-        user = UserModel(username=username, email=email, password=password)
-
-        UserModel.add_user(user.user_as_dict())
-
-        response = jsonify({
-            'status': 201,
-            'data': [
-                {
-                    'id': user.get_user_id(),
-                    'message': "Create user record"
-                }
-            ]
-        })
-        response.status_code = 201
-        return response
+    app.register_blueprint(AUTH)
 
     @app.route('/auth/login', methods=['POST'])
     def user_login():
