6b42d7a216fb32818a1decc9e96167767cd2da31
khwilo <khwilowatai@gmail.com>
2018-12-18 12:33:05 +0300

    [Starts #162697938] Add tests for fetching all questions

--- app/tests/v1/test_question.py
+++ app/tests/v1/test_question.py
@@ -16,3 +16,14 @@ class QuestionTestCase(BaseTestCase):
         response_msg = json.loads(res.data.decode("UTF-8"))
         self.assertEqual(201, response_msg["status"])
         self.assertTrue(response_msg["data"])
+
+    def test_fetch_all_questions(self):
+        '''Test the API can fetch all questions'''
+        res = self.client().post(
+            '/api/v1/questions',
+            headers=BaseTestCase.get_accept_content_type_headers(),
+            data=json.dumps(self.question)
+        )
+        self.assertEqual(res.status_code, 201)
+        res = self.client().get('/api/v1/questions')
+        self.assertEqual(res.status_code, 200)
