e5127459e52d1366a9991a6428247addc0fba8e0
khwilo <khwilowatai@gmail.com>
2018-12-24 14:47:59 +0300

    [Feature #162832583] Import JWTManager class and initialize JWT

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 import json
 
 from flask import Flask, request, jsonify, abort, make_response
+from flask_jwt_extended import JWTManager
 
 from instance.config import APP_CONFIG
 from app.api.v1.models.user_model import UserModel
@@ -12,6 +13,7 @@ from app.api.v1.views.question_view import API
 def create_app(config_name):
     '''Instantiate the Flask application'''
     app = Flask(__name__, instance_relative_config=True)
+    jwt = JWTManager(app)
     app.config.from_object(APP_CONFIG[config_name])
     app.config.from_pyfile('config.py')
     app.register_blueprint(AUTH)
