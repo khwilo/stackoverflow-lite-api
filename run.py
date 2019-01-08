2b48ee475300bd9b9f0221bbf0d9ce407c519469
khwilo <khwilowatai@gmail.com>
2019-01-08 19:45:48 +0300

    Add comment for specifying app settings in production

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 
 from app import create_app
 
-CONFIG_NAME = os.getenv('APP_SETTINGS')
+CONFIG_NAME = os.getenv('APP_SETTINGS') # Specify the app settings in production
 
 APP = create_app(CONFIG_NAME) # Application name
 
